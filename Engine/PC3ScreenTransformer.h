df3eaf20263893784b35eba00786438b8b51394b
Arthur_Prediger <arthurpredigerrambo@gmail.com>
2022-03-01 11:30:35 -0300

    Perspective correct attribute interpolation added

--- Engine/PC3ScreenTransformer.h
+++ Engine/PC3ScreenTransformer.h
@@ -11,16 +11,21 @@ public:
 		xFactor(float(Graphics::ScreenWidth) / 2.0f),
 		yFactor(float(Graphics::ScreenHeight) / 2.0f)
 	{}
-	Vec3& Transform(Vec3& v) const
+	template<class Vertex>
+	Vertex& Transform(Vertex& v) const
 	{
-		const float zInvert = 1.0f / v.z;
-		v.x = (v.x * zInvert + 1.0f) * xFactor;
-		v.y = (-v.y * zInvert + 1.0f) * yFactor;
+		const float zInv = 1.0f / v.pos.z;
+		v *= zInv;
+		v.pos.x = (v.pos.x + 1.0f) * xFactor;
+		v.pos.y = (-v.pos.y + 1.0f) * yFactor;
+		v.pos.z = zInv;
+
 		return v;
 	}
-	Vec3 GetTransformed(const Vec3& v) const
+	template<class Vertex>
+	Vertex GetTransformed(const Vertex& v) const
 	{
-		return Transform(Vec3(v));
+		return Transform(Vertex(v));
 	}
 private:
 	float xFactor;
